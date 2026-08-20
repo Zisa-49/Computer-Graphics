316072115082ea54c9ce7f67c9069ab43aca1641
Suraiya Jahan <suraiyajz31@gmail.com>
2026-08-20 18:10:36 +0100

    Update breshman.cpp

--- breshman.cpp
+++ breshman.cpp
@@ -2,51 +2,51 @@
 #include <graphics.h>
 #include <cstdlib>
 using namespace std;
-void drawLine(int x0, int y0, int x1, int y1, int cx, int cy)
+void drawLine(int x1, int y1, int x2, int y2, int cx, int cy)
 {
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-    int sx = (x0 < x1) ? 1 : -1;
-    int sy = (y0 < y1) ? 1 : -1;
-    int err = dx - dy;
-    while (true)
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    // Initial decision parameter
+    int p = 2 * dy - dx;
+    int x = x1;
+    int y = y1;
+    while (x <= x2)
     {
-        // Mathematical coordinate → screen coordinate
-        putpixel(cx + x0, cy - y0, WHITE);
-        if (x0 == x1 && y0 == y1)
-            break;
-        int e = 2 * err;
-        if (e > -dy)
+        // Mathematical coordinate -> Screen coordinate
+        putpixel(cx + x, cy - y, WHITE);
+        // Case 1: P < 0
+        if (p < 0)
         {
-            err = err - dy;
-            x0 = x0 + sx;
+            x = x + 1;
+            p = p + 2 * dy;
         }
-        if (e < dx)
+        // Case 2: P >= 0
+        else
         {
-            err = err + dx;
-            y0 = y0 + sy;
+            x = x + 1;
+            y = y + 1;
+            p = p + 2 * dy - 2 * dx;
         }
     }
 }
 int main()
 {
-    int x0, y0, x1, y1;
-    // 4 inputs
-    cout << "Enter x0 y0: ";
-    cin >> x0 >> y0;
+    int x1, y1, x2, y2;
     cout << "Enter x1 y1: ";
     cin >> x1 >> y1;
+    cout << "Enter x2 y2: ";
+    cin >> x2 >> y2;
     int gd = DETECT, gm;
     initgraph(&gd, &gm, (char*)"");
-    // Center of window
+    // Center of graphics window
     int cx = getmaxx() / 2;
     int cy = getmaxy() / 2;
     // X-axis
     line(0, cy, getmaxx(), cy);
     // Y-axis
     line(cx, 0, cx, getmaxy());
-    // Bresenham line
-    drawLine(x0, y0, x1, y1, cx, cy);
+    // Draw Bresenham line
+    drawLine(x1, y1, x2, y2, cx, cy);
     getch();
     closegraph();
     return 0;
